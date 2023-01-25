aa8d027ed6de8e1088200ed9fe38ab19335889a9
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-01-25 11:07:16 +0100

    feat(storefront): add fonts & color palette to tailwind config + placeholder page

--- apps/storefront/src/App.tsx
+++ apps/storefront/src/App.tsx
@@ -1,5 +1,12 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className='h-screen w-screen bg-base flex flex-col items-center justify-center text-base-50'>
+      <h1 className='font-title font-bold text-4xl text-transparent bg-clip-text bg-gradient-to-r from-primary-600 to-secondary-600'>
+        webshop
+      </h1>
+      <p className='text-base-400 italic'>coming soon...</p>
+    </div>
+  );
 };
 
 export default App;
