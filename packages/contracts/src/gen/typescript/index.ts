deaba847a504d633d8a97ec858022759205c068f
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-02-01 20:20:31 +0100

    chore: remove accidentally staged file

--- packages/contracts/src/gen/typescript/index.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-// Generated using typescript-generator version 3.1.1185 on 2023-01-29 22:45:29.
-
-export interface GenericResponse {
-    message: string;
-    code: string;
-    statusCode: number;
-}
