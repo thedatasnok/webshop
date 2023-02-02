1f76233d1ad06e37577aec33bdbc3727f315f547
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-02-02 18:59:43 +0100

    feat(storefront): #12 align horizontal line besides logo with content

--- apps/storefront/src/components/layout/Footer.tsx
+++ apps/storefront/src/components/layout/Footer.tsx
@@ -1,53 +1,87 @@
-import { Logo } from "@webshop/ui";
+import { Logo } from '@webshop/ui';
+import clsx from 'clsx';
 
-const Footer = () => {
-  return <footer>
-    <div className="flex justify-center gap-36">
-      <div>
-        <h2 className="mb-6 font-semibold text-base-50 uppercase">Shortcuts</h2>
-        <ul className="text-base-600">
-          <li className="mb-2">
-            <a href="" className="hover:underline">Home</a>
-          </li>
-          <li className="mb-2">
-            <a href="" className="hover:underline">All products</a>
-          </li>
-          <li>
-            <a href="" className="hover:underline">More stuff</a>
-          </li>
-        </ul>
+interface FooterProps {
+  className?: string;
+}
+
+const Footer: React.FC<FooterProps> = ({ className }) => {
+  return (
+    <footer className={clsx('max-w-screen-xl mx-auto mt-8', className)}>
+      <div className='grid grid-cols-3 justify-items-center mb-4'>
+
+        <div>
+          <h2 className='mb-6 font-title font-semibold text-base-50 uppercase'>
+            Shortcuts
+          </h2>
+          <ul className='text-base-400'>
+            <li className='mb-2'>
+              <a href='' className='hover:underline'>
+                Home
+              </a>
+            </li>
+            <li className='mb-2'>
+              <a href='' className='hover:underline'>
+                All products
+              </a>
+            </li>
+            <li>
+              <a href='' className='hover:underline'>
+                More stuff
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h2 className='mb-6 font-title font-semibold text-base-50 uppercase'>
+            Opening times
+          </h2>
+          <ul className='text-base-400'>
+            <li className='mb-2'>
+              <a>Weekdays: 07:00 - 07:05</a>
+            </li>
+            <li className='mb-2'>
+              <a>Weekends: 08:00 - 08:01</a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h2 className='mb-6 font-title font-semibold text-base-50 uppercase'>
+            Contact info
+          </h2>
+          <ul className='text-base-400'>
+            <li className='mb-2'>
+              <a href='' className='hover:underline'>
+                +47 13371337
+              </a>
+            </li>
+            <li className='mb-2'>
+              <a href='mailto:support@cgg.no' className='hover:underline'>
+                support@cgg.no
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div>
-        <h2 className="mb-6 font-semibold text-base-50 uppercase">Opening times</h2>
-        <ul className="text-base-600">
-          <li className="mb-2">
-            <a>Weekdays: 07:00 - 07:05</a>
-          </li>
-          <li className="mb-2">
-            <a>Weekends: 08:00 - 08:01</a>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <h2 className="mb-6 font-semibold text-base-50 uppercase">Contact info</h2>
-        <ul className="text-base-600">
-          <li className="mb-2">
-            <a href="" className="hover:underline">+47 13371337</a>
-          </li>
-          <li className="mb-2">
-            <a href="mailto:support@cgg.no" className="hover:underline">support@cgg.no</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div className="inline-flex items-center justify-center w-full">
-      <hr className="w-64 h-1 my-8 bg-base-800 border-0"></hr>
-      <div className="w-32 absolute px-4 -translate-x-1/2 bg-white left-1/2 bg-base-900">
-        <Logo variant="small"></Logo>
-      </div>
-    </div>
-    <p className="text-sm text-base-400 text-center">© 2023 CGG™ Certain Rights Reserved.</p>
-  </footer>;
+
+      <section id='logo-divider' className='relative my-2'>
+        <div className='absolute inset-0 flex items-center' aria-hidden='true'>
+          <div className='w-full border-t border-base-600' />
+        </div>
+        <div className='relative flex justify-center'>
+          <div className='w-32 px-4 bg-base'>
+            <Logo variant='small' />
+          </div>
+        </div>
+      </section>
+
+      <p className='text-sm text-base-400 text-center my-2'>
+        &copy; 2023 CGG&trade; Certain Rights Reserved.
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
