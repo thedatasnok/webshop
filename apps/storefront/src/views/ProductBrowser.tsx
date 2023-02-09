dca7e85c69bc4e77d39a2d6ab2d7983022b21611
GBFur <zifergames@gmail.com>
2023-02-09 18:52:20 +0100

    feat(storefront): #14 add product browser mockup

--- apps/storefront/src/views/ProductBrowser.tsx
+++ apps/storefront/src/views/ProductBrowser.tsx
@@ -1,9 +1,53 @@
+import Footer from '@/components/layout/Footer';
 import Header from '@/components/layout/Header';
 
 const ProductBrowser = () => {
   return (
     <div>
       <Header />
+
+      <main>
+        <div className='mx-auto w-full max-w-screen-xl'>
+          <div id='cart-title' className='px-10 py-4'>
+            <h1 className='mb-4 flex text-3xl'>Browse</h1>
+            <p>
+              Explore our newest insane gaming gear that will enhance your aim
+              in CS:GO Source 7 and Half life 3
+            </p>
+          </div>
+
+          <section id='categories'>
+            <div className='mx-auto mt-10 mb-5 grid w-fit grid-cols-4 justify-center justify-items-center gap-x-8 lg:grid-cols-8'>
+              {[...Array(8)].map((_, i) => (
+                <div key={i}>
+                  <div className='bg-base-800 aspect-square h-16 rounded-sm' />
+                  <p className='text-center text-sm font-medium'>category</p>
+                </div>
+              ))}
+            </div>
+          </section>
+
+          <div className='mt-7 flex items-center justify-center gap-2'>
+            <h2 className='md:pr-6 lg:pr-96'>showing x results for ""</h2>
+            <button>sort</button>
+            <button>grid</button>
+            <button>list</button>
+          </div>
+
+          <section id='products'>
+            <div className='mx-auto mt-10 mb-5 grid w-fit grid-cols-1 justify-center justify-items-center gap-y-10 gap-x-6 md:grid-cols-2 lg:grid-cols-3'>
+              {[...Array(9)].map((_, i) => (
+                <div key={i}>
+                  <div className='bg-base-800 aspect-square h-48 rounded-sm' />
+                  <p className='text-center text-sm font-medium'>product</p>
+                </div>
+              ))}
+            </div>
+          </section>
+        </div>
+      </main>
+
+      <Footer />
     </div>
   );
 };
