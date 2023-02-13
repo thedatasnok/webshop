4e857b278e29f5dcab24a590545c28a416ec99f1
GBFur <zifergames@gmail.com>
2023-02-13 11:35:13 +0100

    feat(storefront): removed padding

--- apps/storefront/src/views/ShoppingCart.tsx
+++ apps/storefront/src/views/ShoppingCart.tsx
@@ -8,26 +8,26 @@ const ShoppingCart = () => {
       <Header />
 
       <main>
-        <div className='mx-auto max-w-screen-xl px-20 py-10'>
+        <div className='mx-auto max-w-screen-xl py-10'>
           <div id='cart-title' className='justify-start text-3xl'>
             <h1>Shopping cart</h1>
           </div>
 
           <section id='dummy-cart-items'>
             {[...Array(4)].map((_, i) => (
-              <div className='py-2 flex' key={i}>
-                <div className='h-24 w-48 rounded-sm bg-base-800' />
+              <div className='flex py-2' key={i}>
+                <div className='bg-base-800 h-24 w-48 rounded-sm' />
                 <p className='px-4'>3D GAMING</p>
               </div>
             ))}
           </section>
           <div
             id='cart-total'
-            className='flex flex-col w-full self-end py-6 text-xl'
+            className='flex w-full flex-col self-end py-6 text-xl'
           >
             <a className='self-end'>Total: $112,000</a>
           </div>
-          <div id='checkout' className='flex flex-col w-full text-lg'>
+          <div id='checkout' className='flex w-full flex-col text-lg'>
             <Button className='self-end rounded-sm'>Checkout</Button>
           </div>
         </div>
