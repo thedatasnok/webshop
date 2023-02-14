c7d6e6bac43a56daccd454b28979edcb237ec894
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-02-14 17:41:09 +0100

    fix(backend): #27 add missing no args constructor
    this is required by JPA, any other constructors should probably be defined explicitly

--- apps/backend/src/main/java/no/ntnu/webshop/model/UserAccount.java
+++ apps/backend/src/main/java/no/ntnu/webshop/model/UserAccount.java
@@ -16,6 +16,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
@@ -24,6 +25,7 @@ import lombok.Setter;
  */
 @Getter
 @Entity
+@NoArgsConstructor
 @Table(name = UserAccount.TABLE_NAME)
 public class UserAccount {
   
