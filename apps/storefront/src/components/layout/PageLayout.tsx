62c5e282bd10ee45e7494a8388cc82000610c075
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-02-22 14:24:15 +0100

    fix(storefront): add page padding and remove scroll restoration between page renders

--- apps/storefront/src/components/layout/PageLayout.tsx
+++ apps/storefront/src/components/layout/PageLayout.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import NavigationBar from '../navigation/NavigationBar';
 import Footer from './Footer';
 import Header from './Header';
@@ -11,9 +12,18 @@ interface PageLayoutProps {
  * It's meant to be opt-in as not every page will need it.
  */
 const PageLayout: React.FC<PageLayoutProps> = ({ children }) => {
+
+  /**
+   * Scrolls to the top of the page on render.
+   * Avoids the issue of the scroll position being remembered when navigating between pages.
+   */
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
-      <div className='overflow-auto px-2 sm:px-4'>
+      <div className='overflow-auto px-2 sm:px-4 pb-16'>
         <Header />
 
         {children}
