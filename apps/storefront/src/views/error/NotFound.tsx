fb20d31d0e24937e4ba4f60f8d6e0e746c77d936
GBFur <zifergames@gmail.com>
2023-02-22 11:54:21 +0100

    feat(storefront): mockup not found page

--- apps/storefront/src/views/error/NotFound.tsx
+++ apps/storefront/src/views/error/NotFound.tsx
@@ -1,5 +1,18 @@
+import PageLayout from '@/components/layout/PageLayout';
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <PageLayout>
+      <main>
+        <div className='flex h-96 flex-col items-center justify-center'>
+          <h1 className='font-title flex justify-center text-8xl font-semibold'>
+            404
+          </h1>
+          <p>could not find that page</p>
+        </div>
+      </main>
+    </PageLayout>
+  );
 };
 
 export default NotFound;
