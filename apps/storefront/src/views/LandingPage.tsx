01c6eebee2562abc856244c127eb412163a4e066
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-02-22 11:04:54 +0100

    fix(storefront): repair storefront after using common page layout component

--- apps/storefront/src/views/LandingPage.tsx
+++ apps/storefront/src/views/LandingPage.tsx
@@ -6,18 +6,23 @@ const LandingPage = () => {
   return (
     <PageLayout>
       <main>
-        <section
-          id='hero'
-          className='h-[28rem] w-full bg-cover'
-          style={{
-            backgroundImage:
-              'url(https://images.unsplash.com/photo-1557515126-1bf9ada5cb93?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3131&q=80)',
-          }}
-        >
-          <div className='from-base via-base/20 to-base flex h-full w-full flex-col items-center justify-center gap-4 bg-gradient-to-b'>
-            <h1 className='font-title mt-16 text-center text-4xl font-bold'>
-              Gaming gear that just makes you better, <br /> faster, stronger
-              and more awesome.
+        {/* Hero section is a bit hacky, but it's a one off since it is "below" the header and covers full width */}
+        <section id='hero' className='relative -mb-32 h-96'>
+          <div
+            className='absolute -top-32 -z-10 -ml-2 h-full w-[calc(100%+32px)] bg-cover sm:-ml-4'
+            style={{
+              backgroundImage:
+                'url(https://images.unsplash.com/photo-1557515126-1bf9ada5cb93?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3131&q=80)',
+            }}
+          >
+            <div className='from-base via-base/20 to-base flex h-full w-full flex-col items-center justify-center gap-4 bg-gradient-to-b'></div>
+          </div>
+
+          <div className='absolute top-2 left-1/2 flex w-full -translate-x-1/2 flex-col items-center gap-2 sm:top-8'>
+            <h1 className='font-title text-center text-3xl font-bold'>
+              Gaming gear that just makes you better,{' '}
+              <br className='hidden sm:block' />
+              faster, stronger and more awesome.
             </h1>
             <Button className='font-title rounded-sm text-lg font-bold'>
               Browse shop
@@ -25,6 +30,7 @@ const LandingPage = () => {
           </div>
         </section>
 
+        {/* Main content segment */}
         <div className='mx-auto flex w-full max-w-screen-xl flex-col'>
           <h2 className='font-title my-4 text-center text-2xl font-bold uppercase tracking-wider'>
             Categories
