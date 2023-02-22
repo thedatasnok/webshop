ef0be84d375a1808af2aa3387ca3e2c8d9f1eaa5
GBFur <zifergames@gmail.com>
2023-02-22 12:45:31 +0100

    feat(storefront): #35 wired up footer links

--- apps/storefront/src/components/layout/Footer.tsx
+++ apps/storefront/src/components/layout/Footer.tsx
@@ -1,5 +1,6 @@
 import { Logo } from '@webshop/ui';
 import clsx from 'clsx';
+import { NavLink } from 'react-router-dom';
 
 interface FooterProps {
   className?: string;
@@ -7,34 +8,33 @@ interface FooterProps {
 
 const Footer: React.FC<FooterProps> = ({ className }) => {
   return (
-    <footer className={clsx('max-w-screen-xl mx-auto mt-8', className)}>
-      <div className='grid grid-cols-3 justify-items-center mb-4'>
-
+    <footer className={clsx('mx-auto mt-8 max-w-screen-xl', className)}>
+      <div className='mb-4 grid grid-cols-3 justify-items-center'>
         <div>
-          <h2 className='mb-6 font-title font-semibold text-base-50 uppercase'>
+          <h2 className='font-title text-base-50 mb-6 font-semibold uppercase'>
             Shortcuts
           </h2>
           <ul className='text-base-400'>
             <li className='mb-2'>
-              <a href='' className='hover:underline'>
+              <NavLink to='/' className='hover:underline'>
                 Home
-              </a>
+              </NavLink>
             </li>
             <li className='mb-2'>
-              <a href='' className='hover:underline'>
+              <NavLink to='/products' className='hover:underline'>
                 All products
-              </a>
+              </NavLink>
             </li>
             <li>
-              <a href='' className='hover:underline'>
-                More stuff
-              </a>
+              <NavLink to='/support' className='hover:underline'>
+                Support
+              </NavLink>
             </li>
           </ul>
         </div>
 
         <div>
-          <h2 className='mb-6 font-title font-semibold text-base-50 uppercase'>
+          <h2 className='font-title text-base-50 mb-6 font-semibold uppercase'>
             Opening times
           </h2>
           <ul className='text-base-400'>
@@ -48,7 +48,7 @@ const Footer: React.FC<FooterProps> = ({ className }) => {
         </div>
 
         <div>
-          <h2 className='mb-6 font-title font-semibold text-base-50 uppercase'>
+          <h2 className='font-title text-base-50 mb-6 font-semibold uppercase'>
             Contact info
           </h2>
           <ul className='text-base-400'>
@@ -68,16 +68,16 @@ const Footer: React.FC<FooterProps> = ({ className }) => {
 
       <section id='logo-divider' className='relative my-2'>
         <div className='absolute inset-0 flex items-center' aria-hidden='true'>
-          <div className='w-full border-t border-base-600' />
+          <div className='border-base-600 w-full border-t' />
         </div>
         <div className='relative flex justify-center'>
-          <div className='w-32 px-4 bg-base'>
+          <div className='bg-base w-32 px-4'>
             <Logo variant='small' />
           </div>
         </div>
       </section>
 
-      <p className='text-sm text-base-400 text-center my-2'>
+      <p className='text-base-400 my-2 text-center text-sm'>
         &copy; 2023 CGG&trade; Certain Rights Reserved.
       </p>
     </footer>
