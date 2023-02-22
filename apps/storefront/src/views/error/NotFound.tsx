97db9da8548e0b5c1aa0fad7e553e6e372df24ba
GBFur <zifergames@gmail.com>
2023-02-22 13:01:14 +0100

    feat(storefront): go back button on 404 page

--- apps/storefront/src/views/error/NotFound.tsx
+++ apps/storefront/src/views/error/NotFound.tsx
@@ -1,14 +1,17 @@
 import PageLayout from '@/components/layout/PageLayout';
+import NavigationItem from '@/components/navigation/NavigationItem';
+import { RiArrowGoBackFill } from 'react-icons/ri';
 
 const NotFound = () => {
   return (
     <PageLayout>
       <main>
         <div className='flex h-96 flex-col items-center justify-center'>
-          <h1 className='font-title flex justify-center text-8xl font-semibold'>
-            404
-          </h1>
+          <h1 className='font-title flex text-8xl font-semibold'>404</h1>
           <p>could not find that page</p>
+          <div className='py-4'>
+            <NavigationItem to='/' name='go back' icon={RiArrowGoBackFill} />
+          </div>
         </div>
       </main>
     </PageLayout>
