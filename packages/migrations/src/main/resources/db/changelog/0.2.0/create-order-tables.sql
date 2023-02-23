73bd61454fd6d1d9e256872daf28a8f76d457575
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-02-23 18:23:07 +0100

    feat(migrations): #28 define remaining tables and types

---
+++ packages/migrations/src/main/resources/db/changelog/0.2.0/create-order-tables.sql
@@ -0,0 +1,59 @@
+CREATE TYPE webshop_payment_method AS ENUM (
+  'CREDIT_CARD',
+  'PAYPAL',
+  'BANK_TRANSFER',
+  'CASH_ON_DELIVERY'
+);
+
+CREATE TYPE webshop_order_status AS ENUM (
+  'NEW',
+  'IN_PROGRESS',
+  'SHIPPED',
+  'DELIVERED',
+  'CANCELLED'
+);
+
+CREATE TYPE webshop_payment_status AS ENUM (
+  'NEW',
+  'IN_PROGRESS',
+  'PAID',
+  'CANCELLED'
+);
+
+CREATE TABLE order (
+  order_id BIGINT GENERATED ALWAYS AS IDENTITY,
+  total NUMERIC NOT NULL,
+  order_status webshop_order_status NOT NULL,
+  payment_status webshop_payment_status NOT NULL,
+  payment_method webshop_payment_method NOT NULL,
+  fk_customer_id UUID NOT NULL,
+  customer_name TEXT,
+
+  -- Delivery address
+  delivery_country TEXT,
+  delivery_postal_code TEXT,
+  delivery_city TEXT,
+  delivery_street TEXT,
+  delivery_care_of TEXT,
+
+  -- Invoice address
+  invoice_country TEXT,
+  invoice_postal_code TEXT,
+  invoice_city TEXT,
+  invoice_street TEXT,
+  invoice_care_of TEXT,
+
+  PRIMARY KEY (order_id),
+  FOREIGN KEY (fk_user_account_id) REFERENCES user_account(user_account_id),
+  FOREIGN KEY (fk_product_price_id) REFERENCES product_price(product_price_id)
+);
+
+CREATE TABLE order_line (
+  order_line_id BIGINT GENERATED ALWAYS AS IDENTITY,
+  fk_order_id BIGINT NOT NULL,
+  fk_product_id BIGINT NOT NULL,
+  fk_product_price_id BIGINT NOT NULL,
+  quantity INT NOT NULL,
+  subtotal NUMERIC
+);
+
