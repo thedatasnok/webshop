35d911b26e77ca1f92859ad460bc4e369285465a
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-03-01 14:21:26 +0100

    refactor(migrations): use postgres enum for user account roles

--- packages/migrations/src/main/resources/db/changelog/0.1.0/create-user-account-role-table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE user_account_role (
-  role_id INT GENERATED ALWAYS AS IDENTITY,
-  name TEXT UNIQUE NOT NULL,
-
-  PRIMARY KEY (role_id)
-);
--- packages/migrations/src/main/resources/db/changelog/0.1.0/create-user-account-table.sql
+++ packages/migrations/src/main/resources/db/changelog/0.1.0/create-user-account-table.sql
@@ -1,13 +1,14 @@
+CREATE TYPE user_account_role AS ENUM ('SHOP_OWNER', 'SHOP_WORKER', 'CUSTOMER');
+
 CREATE TABLE user_account (
   user_account_id UUID DEFAULT uuid_generate_v4(),
   full_name TEXT NOT NULL,
   email TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL,
   email_verified BOOLEAN NOT NULL DEFAULT FALSE,
-  fk_role_id INT NOT NULL,
+  role user_account_role NOT NULL,
   created_at TIMESTAMP DEFAULT now(),
   updated_at TIMESTAMP,
 
-  PRIMARY KEY (user_account_id),
-  FOREIGN KEY (fk_role_id) REFERENCES user_account_role(role_id)
+  PRIMARY KEY (user_account_id)
 );
