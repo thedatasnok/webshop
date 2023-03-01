3dd594ac37440094da85d85eac80e1ed9e06b662
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-03-01 16:29:26 +0100

    style(contracts): remove unused imports

--- packages/contracts/src/main/java/no/ntnu/webshop/contracts/auth/SignUpRequest.java
+++ packages/contracts/src/main/java/no/ntnu/webshop/contracts/auth/SignUpRequest.java
@@ -1,11 +1,8 @@
 package no.ntnu.webshop.contracts.auth;
 
-import org.springframework.boot.context.properties.bind.Name;
-
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import no.ntnu.webshop.contracts.utility.annotation.GenerateTypeScript;
 import no.ntnu.webshop.contracts.validation.annotation.ValidPassword;
 
