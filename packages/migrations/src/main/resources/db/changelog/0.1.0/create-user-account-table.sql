3c48ac71a201af9dce87dc39d8b935a3c902ddb0
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-03-01 15:45:54 +0100

    refactor(migrations): use `pgcrypto` instead of `uuid-ossp`
    pgcrypto is more up to date

--- packages/migrations/src/main/resources/db/changelog/0.1.0/create-user-account-table.sql
+++ packages/migrations/src/main/resources/db/changelog/0.1.0/create-user-account-table.sql
@@ -1,7 +1,7 @@
 CREATE TYPE user_account_role AS ENUM ('SHOP_OWNER', 'SHOP_WORKER', 'CUSTOMER');
 
 CREATE TABLE user_account (
-  user_account_id UUID DEFAULT uuid_generate_v4(),
+  user_account_id UUID DEFAULT gen_random_uuid(),
   full_name TEXT NOT NULL,
   email TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL,
