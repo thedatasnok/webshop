59323ccf03b37f039a7b850135d8f3078735abe9
Dawid Staniszewski <dstaniszewski00@gmail.com>
2023-03-01 14:42:53 +0100

    feat(storefront): #36 create a simple TextInput component

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,5 +1,6 @@
 import Logo from './branding/Logo';
 import Corner from './decoration/Corner';
 import Button from './input/Button';
+import TextField from './input/TextField';
 
-export { Button, Corner, Logo };
+export { Button, Corner, Logo, TextField };
