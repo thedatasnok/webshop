d9782ad845cf27fdcd153e9f7291f801f2f15087
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-03-05 21:48:21 +0100

    feat(storefront): code split routes

--- apps/storefront/src/main.tsx
+++ apps/storefront/src/main.tsx
@@ -2,14 +2,16 @@ import router from '@/router';
 import '@webshop/ui/styles.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { RouterProvider } from 'react-router-dom';
 import { store } from './store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <React.Suspense>
+        <RouterProvider router={router} />
+      </React.Suspense>
     </Provider>
   </React.StrictMode>
 );
