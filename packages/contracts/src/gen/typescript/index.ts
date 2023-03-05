5ac7dca27e779961d98e548197431e6814adcab5
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-03-05 14:12:10 +0100

    fix(contracts): remove top most comment to avoid unnecessary commits when content doesn't change
    the time can be approximated through git logs if needed

--- packages/contracts/src/gen/typescript/index.ts
+++ packages/contracts/src/gen/typescript/index.ts
@@ -1,6 +1,5 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.1.1185 on 2023-03-05 13:08:37.
 
 export interface GenericResponse {
     message: string;
