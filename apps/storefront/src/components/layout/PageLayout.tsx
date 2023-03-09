d5568bf27eeb5f1f72074999f6b6a3b7de16efd2
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-03-09 23:21:30 +0100

    fix(storefront): scroll to top when location changes
    this will fix the issue when selecting a new product from the related products section a product view

--- apps/storefront/src/components/layout/PageLayout.tsx
+++ apps/storefront/src/components/layout/PageLayout.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import NavigationBar from '../navigation/NavigationBar';
 import Footer from './Footer';
 import Header from './Header';
@@ -13,13 +14,15 @@ interface PageLayoutProps {
  * It's meant to be opt-in as not every page will need it.
  */
 const PageLayout: React.FC<PageLayoutProps> = ({ excludeHeader, children }) => {
+  const location = useLocation();
+
   /**
    * Scrolls to the top of the page on render.
    * Avoids the issue of the scroll position being remembered when navigating between pages.
    */
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, []);
+  }, [location]);
 
   return (
     <>
