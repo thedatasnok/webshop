119c3b625724389c993d15f5cd34723bb9eb94e6
dawsta27 <99175000+dawsta27@users.noreply.github.com>
2023-03-09 11:49:35 +0100

    feat(backend): #55 set up endpoint for retrieving a user's profile

--- apps/backend/src/main/java/no/ntnu/webshop/repository/UserAccountJpaRepository.java
+++ apps/backend/src/main/java/no/ntnu/webshop/repository/UserAccountJpaRepository.java
@@ -4,13 +4,32 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import no.ntnu.webshop.contracts.user.UserProfile;
 import no.ntnu.webshop.model.UserAccount;
 
 public interface UserAccountJpaRepository extends JpaRepository<UserAccount, UUID> {
 
   Optional<UserAccount> findByEmail(
       String email
+  );
+
+  @Query("""
+    SELECT new no.ntnu.webshop.contracts.user.UserProfile(
+      user.id,
+      user.fullName,
+      user.email,
+      user.emailVerified,
+      CAST(user.role AS STRING),
+      user.createdAt
+    )
+    FROM UserAccount user
+    WHERE user.id = :id
+    """)
+  UserProfile findProfile(
+      @Param("id") UUID id
   );
 
 }
