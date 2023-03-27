d6f8673ee7695bff63fb6a4c4dd4f9707026fc2e
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-03-27 18:56:24 +0200

    fix: update password validation logic to consider null values valid & refactor updating user profile

--- packages/contracts/src/main/java/no/ntnu/webshop/contracts/auth/SignUpRequest.java
+++ packages/contracts/src/main/java/no/ntnu/webshop/contracts/auth/SignUpRequest.java
@@ -2,16 +2,16 @@ package no.ntnu.webshop.contracts.auth;
 
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import no.ntnu.webshop.contracts.utility.annotation.GenerateTypeScript;
 import no.ntnu.webshop.contracts.validation.annotation.ValidPassword;
 
 @GenerateTypeScript
 public record SignUpRequest(
-  @NotNull String fullName,
-  @NotNull @Email String email,
-  @NotNull @ValidPassword String password,
-  @NotNull String passwordConfirmation
+  @NotBlank String fullName,
+  @NotBlank @Email String email,
+  @NotBlank @ValidPassword String password,
+  @NotBlank String passwordConfirmation
 ) {
 
   @AssertTrue
