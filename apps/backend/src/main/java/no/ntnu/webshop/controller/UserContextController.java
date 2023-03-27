0fcb6400663b87fd3602016a39361b48c22507b3
dawsta27 <99175000+dawsta27@users.noreply.github.com>
2023-03-27 11:58:57 +0200

    feat(backend): #75 add endpoint for updating user details

--- apps/backend/src/main/java/no/ntnu/webshop/controller/UserContextController.java
+++ apps/backend/src/main/java/no/ntnu/webshop/controller/UserContextController.java
@@ -1,14 +1,23 @@
 package no.ntnu.webshop.controller;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
 import lombok.RequiredArgsConstructor;
+import no.ntnu.webshop.contracts.user.UpdateUserProfileRequest;
 import no.ntnu.webshop.contracts.user.UserProfile;
 import no.ntnu.webshop.repository.UserAccountJpaRepository;
 import no.ntnu.webshop.security.UserAccountDetailsAdapter;
@@ -18,7 +27,9 @@ import no.ntnu.webshop.security.UserAccountDetailsAdapter;
 @RequestMapping("/api/v1/me")
 public class UserContextController {
   private final UserAccountJpaRepository userAccountJpaRepository;
-  
+  private final PasswordEncoder passwordEncoder;
+  private final Validator validator;
+
   @Operation(summary = "Returns the user profile of the logged in user")
   @GetMapping
   @PreAuthorize("isAuthenticated()")
@@ -27,6 +38,39 @@ public class UserContextController {
   ) {
     var userId = adapter.getUserAccount().getId();
     return ResponseEntity.ok(this.userAccountJpaRepository.findProfile(userId));
+  }
+
+  @Operation(summary = "Updates the user account information")
+  @PatchMapping
+  @PreAuthorize("isAuthenticated()")
+  public ResponseEntity<UserProfile> updateProfile(
+      @AuthenticationPrincipal UserAccountDetailsAdapter adapter,
+      @RequestBody UpdateUserProfileRequest request
+  ) {
+    var user = adapter.getUserAccount();
+
+    Set<ConstraintViolation<UpdateUserProfileRequest>> violations = new HashSet<>();
+
+    violations.addAll(this.validator.validateProperty(request, "fullName"));
+    violations.addAll(this.validator.validateProperty(request, "email"));
+
+    user.setFullName(request.fullName());
+    user.setEmail(request.email());
+
+    if (request.validatePasswords()) {
+      if (!request.passwordsMatch())
+        throw new IllegalArgumentException();
+
+      violations.addAll(this.validator.validateProperty(request, "password"));
+      user.setPassword(this.passwordEncoder.encode(request.password()));
+    }
+
+    if (!violations.isEmpty())
+      throw new IllegalArgumentException();
+
+    this.userAccountJpaRepository.save(user);
+
+    return ResponseEntity.ok(this.userAccountJpaRepository.findProfile(user.getId()));
   }
 
 }
--- packages/contracts/src/main/java/no/ntnu/webshop/contracts/user/UpdateUserProfileRequest.java
+++ packages/contracts/src/main/java/no/ntnu/webshop/contracts/user/UpdateUserProfileRequest.java
@@ -1,6 +1,28 @@
 package no.ntnu.webshop.contracts.user;
 
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import no.ntnu.webshop.contracts.utility.annotation.GenerateTypeScript;
+import no.ntnu.webshop.contracts.validation.annotation.ValidPassword;
 
 @GenerateTypeScript
-public record UpdateUserProfileRequest(String fullName, String email, String password, String passwordConfirmation) {}
+public record UpdateUserProfileRequest(
+  @NotBlank String fullName,
+  @NotBlank @Email String email,
+  @ValidPassword String password,
+  @ValidPassword String passwordConfirmation
+) {
+
+  public boolean validatePasswords() {
+    return this.password != null || this.passwordConfirmation != null;
+  }
+
+  @AssertTrue
+  public boolean passwordsMatch() {
+    if (this.password == null || this.passwordConfirmation == null) return false;
+
+    return this.password.equals(this.passwordConfirmation);
+  }
+
+}
