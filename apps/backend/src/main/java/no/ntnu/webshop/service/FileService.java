76e46de552fc71b052d60bedb7d312b37655521c
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-03-29 21:55:16 +0200

    feat(backend): add support for uploading files to a S3 bucket
    meant to be used for uploading static assets, such as product images and category icons

---
+++ apps/backend/src/main/java/no/ntnu/webshop/service/FileService.java
@@ -0,0 +1,72 @@
+package no.ntnu.webshop.service;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Service for uploading files to S3.
+ */
+@Slf4j
+@Service
+public class FileService {
+  private AmazonS3 client;
+  private String bucketName;
+
+  public FileService(
+      @Value("${no.ntnu.webshop.s3.access-key}") String accessKey,
+      @Value("${no.ntnu.webshop.s3.secret-access-key}") String secretAccessKey,
+      @Value("${no.ntnu.webshop.s3.region-name}") String regionName,
+      @Value("${no.ntnu.webshop.s3.bucket-name}") String bucketName
+  ) {
+    var credentials = new BasicAWSCredentials(
+      accessKey,
+      secretAccessKey
+    );
+
+    this.client = AmazonS3ClientBuilder.standard()
+      .withCredentials(new AWSStaticCredentialsProvider(credentials))
+      .withRegion(regionName)
+      .build();
+
+    this.bucketName = bucketName;
+  }
+
+  /**
+   * Uploads a file to the configured S3 bucket.
+   * 
+   * @param file     the file to upload
+   * @param fileName the name of the file
+   * 
+   * @return the URL of the uploaded file
+   */
+  public String uploadFile(
+      MultipartFile file,
+      String fileName
+  ) throws IOException {
+    var metadata = new ObjectMetadata();
+    metadata.setContentType(file.getContentType());
+    var key = "images/".concat(fileName);
+
+    // only proceed to upload if image does not already exist
+    // could probably just put it anyway, but this prevents replacing existing images
+    // TODO: catch exceptions for remote calls
+    if (!this.client.doesObjectExist(this.bucketName, key)) {
+      this.client.putObject(this.bucketName, key, file.getInputStream(), metadata);
+      log.debug("Successfully uploaded image to S3 bucket");
+    }
+
+    return this.client.getUrl(this.bucketName, key).toString();
+  }
+
+}
