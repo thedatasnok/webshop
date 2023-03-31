8535e4174c00afdb9f9798c91c65d13a700589fa
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-03-31 23:38:19 +0200

    feat: add sample mail setup using `react-email`

---
+++ apps/mail/src/env.ts
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+import { z } from 'zod';
+
+dotenv.config();
+
+console.log(process.env);
+
+/**
+ * Schema for validatinge environment variables.
+ */
+const environmentSchema = z.object({
+  hostname: z.string(),
+  port: z.string().transform((port) => parseInt(port)),
+  username: z.string(),
+  password: z.string(),
+  sender: z.string().email(),
+});
+
+const environment = environmentSchema.parse({
+  hostname: process.env.MAIL_SERVER_HOSTNAME,
+  port: process.env.MAIL_SERVER_PORT,
+  username: process.env.MAIL_USERNAME,
+  password: process.env.MAIL_PASSWORD,
+  sender: process.env.MAIL_SENDER,
+});
+
+export default environment;
