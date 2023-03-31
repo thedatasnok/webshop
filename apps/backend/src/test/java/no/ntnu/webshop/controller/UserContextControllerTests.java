8e0f46bd8fdf1a6fad3395535ca89b99efc17797
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-03-31 23:17:20 +0200

    test: add test asserting the profile, including role changes
    this inherently also verifies that authentication works

---
+++ apps/backend/src/test/java/no/ntnu/webshop/controller/UserContextControllerTests.java
@@ -0,0 +1,112 @@
+package no.ntnu.webshop.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.RequiredArgsConstructor;
+import no.ntnu.webshop.contracts.auth.SignInRequest;
+import no.ntnu.webshop.contracts.auth.SignInResponse;
+import no.ntnu.webshop.model.UserAccount;
+import no.ntnu.webshop.model.UserAccountRole;
+import no.ntnu.webshop.repository.UserAccountJpaRepository;
+
+@SpringBootTest
+@RequiredArgsConstructor
+public class UserContextControllerTests {
+  private final UserAccountJpaRepository userAccountJpaRepository;
+  private final PasswordEncoder passwordEncoder;
+  private final WebApplicationContext context;
+  private final ObjectMapper objectMapper;
+
+  private static final String fullName = "Bob";
+  private static final String email = "bob@example.com";
+  private static final String password = "password";
+
+  private MockMvc mockMvc;
+  private UserAccount user;
+
+  @BeforeEach
+  void setup() {
+    var account = new UserAccount(
+      fullName,
+      email,
+      this.passwordEncoder.encode(password),
+      UserAccountRole.CUSTOMER
+    );
+
+    user = this.userAccountJpaRepository.save(account);
+    this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+      .apply(SecurityMockMvcConfigurers.springSecurity())
+      .build();
+  }
+
+  @AfterEach
+  void cleanup() {
+    this.userAccountJpaRepository.delete(user);
+  }
+
+  /**
+   * Tests that the user profile is evaluated correctly, not being inferred from token.y
+   */
+  @Test
+  void userProfileReflectsChanges() throws Exception {
+    var signInRequest = new SignInRequest(
+      email,
+      password
+    );
+
+    var signInResult = this.mockMvc
+      .perform(
+        MockMvcRequestBuilders.post("/api/v1/auth/sign-in")
+          .contentType(MediaType.APPLICATION_JSON)
+          .content(this.objectMapper.writeValueAsString(signInRequest))
+      )
+      .andReturn();
+
+    var token = this.objectMapper.readValue(signInResult.getResponse().getContentAsString(), SignInResponse.class)
+      .accessToken();
+
+    this.mockMvc
+      .perform(
+        MockMvcRequestBuilders.get("/api/v1/me")
+          .contentType(MediaType.APPLICATION_JSON)
+          .header("Authorization", "Bearer ".concat(token))
+      )
+      .andExpect(MockMvcResultMatchers.status().isOk())
+      .andExpect(MockMvcResultMatchers.jsonPath("$.id").isString())
+      .andExpect(MockMvcResultMatchers.jsonPath("$.fullName", Matchers.is(fullName)))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.email", Matchers.is(email)))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.role", Matchers.is("CUSTOMER")));
+
+    this.userAccountJpaRepository.findByEmail(email).ifPresent(account -> {
+      account.setRole(UserAccountRole.SHOP_OWNER);
+      this.userAccountJpaRepository.save(account);
+    });
+
+    this.mockMvc
+      .perform(
+        MockMvcRequestBuilders.get("/api/v1/me")
+          .contentType(MediaType.APPLICATION_JSON)
+          .header("Authorization", "Bearer " + token)
+      )
+      .andExpect(MockMvcResultMatchers.status().isOk())
+      .andExpect(MockMvcResultMatchers.jsonPath("$.id").isString())
+      .andExpect(MockMvcResultMatchers.jsonPath("$.fullName", Matchers.is(fullName)))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.email", Matchers.is(email)))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.role", Matchers.is("SHOP_OWNER")));
+  }
+
+}
