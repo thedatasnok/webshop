af52cd2765d68ebe8f9f59f3c0b66508faa0e1b0
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-04-12 21:11:51 +0200

    feat(storefront): parse response header for retrieving the id of a placed order from the response headers

--- apps/storefront/src/services/userContextOrders.ts
+++ apps/storefront/src/services/userContextOrders.ts
@@ -6,11 +6,21 @@ export const userContextOrdersApi = webshopApi.injectEndpoints({
     /**
      * Places an order in the current user's context.
      */
-    placeOrder: builder.mutation<void, PlaceOrderRequest>({
+    placeOrder: builder.mutation<number, PlaceOrderRequest>({
       query: (body) => ({
         url: '/v1/me/orders',
         method: 'POST',
         body,
+        async responseHandler(response) {
+          if (response.headers.has('location')) {
+            const location = response.headers.get('location');
+            return Number(location?.split('/').pop());
+          }
+
+          // the API should always return a location header with the order id encoded in it
+          // but if it doesn't, we'll return -1 for now
+          return -1;
+        },
       }),
     }),
   }),
