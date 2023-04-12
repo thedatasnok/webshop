79475d6147b914bde51db3ada09f9667d53a1d41
GBFur <zifergames@gmail.com>
2023-04-12 08:36:07 +0200

    fix: changed request body import

--- apps/backend/src/main/java/no/ntnu/webshop/controller/UserContextOrderController.java
+++ apps/backend/src/main/java/no/ntnu/webshop/controller/UserContextOrderController.java
@@ -9,12 +9,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
