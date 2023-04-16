1c15cfcbc43c6559eebbe7e95a43406d8d544bf9
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-04-16 16:42:42 +0200

    refactor(storefront): remove unnecessary wrapping div

--- apps/storefront/src/views/ProductBrowser.tsx
+++ apps/storefront/src/views/ProductBrowser.tsx
@@ -69,120 +69,120 @@ const ProductBrowser = () => {
 
   return (
     <PageLayout>
-      <main>
-        <div className='mx-auto w-full max-w-screen-xl'>
-          <div id='cart-title' className='py-4'>
-            <h1 className='font-title mb-2 flex text-2xl font-semibold uppercase'>
-              Browse
-            </h1>
+      <main className='mx-auto w-full max-w-screen-xl'>
+        <section id='hero' className='py-4'>
+          <h1 className='font-title mb-2 flex text-2xl font-semibold uppercase'>
+            Browse
+          </h1>
 
-            <p className='text-base-300 text-sm'>
-              Explore our newest insane gaming gear that will enhance your aim
-              in CS:GO Source 7 and Half life 3
-            </p>
+          <p className='text-base-300 text-sm'>
+            Explore our newest insane gaming gear that will enhance your aim in
+            CS:GO Source 7 and Half life 3
+          </p>
+        </section>
+
+        <section id='categories'>
+          <div className='mx-auto my-2 grid w-full grid-cols-4 gap-2 lg:grid-cols-8'>
+            {categories?.map((category) => (
+              <button
+                onClick={() => handleCategoryClick(category)}
+                key={category.id}
+              >
+                <CategoryCard
+                  id={category.id}
+                  name={category.name}
+                  iconUrl={category.iconUrl}
+                  selected={parsedCategoryId === category.id}
+                />
+              </button>
+            ))}
           </div>
-          <section id='categories'>
-            <div className='mx-auto my-2 grid w-full grid-cols-4 gap-2 lg:grid-cols-8'>
-              {categories?.map((category) => (
-                <button
-                  onClick={() => handleCategoryClick(category)}
-                  key={category.id}
-                >
-                  <CategoryCard
-                    id={category.id}
-                    name={category.name}
-                    iconUrl={category.iconUrl}
-                    selected={parsedCategoryId === category.id}
-                  />
-                </button>
-              ))}
-            </div>
-          </section>
-          <div className='mt-2 flex flex-col-reverse sm:flex-row sm:items-center sm:justify-between'>
-            <h2
-              className={clsx(
-                'text-base-200 font-title text-sm font-semibold uppercase tracking-wide',
-                !paramSearch && 'invisible'
-              )}
-            >
-              Showing {products?.length} results for "{paramSearch}"
-            </h2>
+        </section>
 
-            <div className='my-2 mr-0 flex items-center sm:gap-2'>
-              <span className='text-base-200 font-title text-sm font-semibold uppercase tracking-wide'>
-                Sorting by
-              </span>
-              <SortByField
-                direction={paramSortDirection}
-                field={paramSortField}
-                fields={PRODUCT_SORT_FIELDS}
-                onChange={onSortChange}
-              />
+        <div className='mt-2 flex flex-col-reverse sm:flex-row sm:items-center sm:justify-between'>
+          <h2
+            className={clsx(
+              'text-base-200 font-title text-sm font-semibold uppercase tracking-wide',
+              !paramSearch && 'invisible'
+            )}
+          >
+            Showing {products?.length} results for "{paramSearch}"
+          </h2>
 
-              <div className='flex items-center'>
-                <button
-                  onClick={handleGridButtonClick}
-                  className={clsx(
-                    'border-base-700 hover:bg-base-800 rounded-l-sm border transition-colors',
-                    isGridSelected && 'text-primary'
-                  )}
-                >
-                  <RiGridFill className='h-7 w-7' />
-                </button>
+          <div className='my-2 mr-0 flex items-center sm:gap-2'>
+            <span className='text-base-200 font-title text-sm font-semibold uppercase tracking-wide'>
+              Sorting by
+            </span>
+            <SortByField
+              direction={paramSortDirection}
+              field={paramSortField}
+              fields={PRODUCT_SORT_FIELDS}
+              onChange={onSortChange}
+            />
 
-                <button
-                  onClick={handleListButtonClick}
-                  className={clsx(
-                    'border-base-700 hover:bg-base-800 rounded-l-sm border transition-colors',
-                    !isGridSelected && 'text-primary'
-                  )}
-                >
-                  <RiListCheck className='h-7 w-7' />
-                </button>
-              </div>
+            <div className='flex items-center'>
+              <button
+                onClick={handleGridButtonClick}
+                className={clsx(
+                  'border-base-700 hover:bg-base-800 rounded-l-sm border transition-colors',
+                  isGridSelected && 'text-primary'
+                )}
+              >
+                <RiGridFill className='h-7 w-7' />
+              </button>
+
+              <button
+                onClick={handleListButtonClick}
+                className={clsx(
+                  'border-base-700 hover:bg-base-800 rounded-l-sm border transition-colors',
+                  !isGridSelected && 'text-primary'
+                )}
+              >
+                <RiListCheck className='h-7 w-7' />
+              </button>
             </div>
           </div>
-
-          <section id='products'>
-            <div
-              className={clsx({
-                'grid grid-cols-2 gap-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5':
-                  isGridSelected,
-                'mx-auto grid grid-cols-1': !isGridSelected,
-              })}
-            >
-              {products?.map((product, i) => (
-                <div key={product.id}>
-                  {isGridSelected ? (
-                    <ProductCard
-                      id={product.id}
-                      to={'/products/' + product.id}
-                      name={product.name}
-                      previousPrice={product.previousPrice}
-                      price={product.price}
-                      shortDescription={product.shortDescription}
-                      isDiscount={product.isDiscount}
-                      image={product.imageUrls[0]}
-                    />
-                  ) : (
-                    <ProductListCard
-                      to={'/products/' + product.id}
-                      name={product.name}
-                      shortDescription={product.shortDescription}
-                      image={product.imageUrls[0]}
-                    >
-                      <ProductListActions
-                        price={product.price}
-                        isDiscount={product.isDiscount}
-                        previousPrice={product.previousPrice}
-                      />
-                    </ProductListCard>
-                  )}
-                </div>
-              ))}
-            </div>
-          </section>
         </div>
+
+        <section id='products'>
+          <div
+            className={clsx({
+              'grid grid-cols-2 gap-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5':
+                isGridSelected,
+              'mx-auto grid grid-cols-1': !isGridSelected,
+            })}
+          >
+            {products?.map((product, i) => (
+              <div key={product.id}>
+                {isGridSelected ? (
+                  <ProductCard
+                    id={product.id}
+                    to={'/products/' + product.id}
+                    name={product.name}
+                    previousPrice={product.previousPrice}
+                    price={product.price}
+                    shortDescription={product.shortDescription}
+                    isDiscount={product.isDiscount}
+                    image={product.imageUrls[0]}
+                  />
+                ) : (
+                  <ProductListCard
+                    to={'/products/' + product.id}
+                    name={product.name}
+                    shortDescription={product.shortDescription}
+                    image={product.imageUrls[0]}
+                  >
+                    <ProductListActions
+                      price={product.price}
+                      isDiscount={product.isDiscount}
+                      previousPrice={product.previousPrice}
+                    />
+                  </ProductListCard>
+                )}
+              </div>
+            ))}
+          </div>
+        </section>
       </main>
     </PageLayout>
   );
