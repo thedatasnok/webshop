454d058f1575ed4b3f099ed08b08ca546420ea2e
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-04-18 23:04:52 +0200

    feat(backend): #120 add endpoint for deleting the currently logged in user

--- apps/backend/src/main/java/no/ntnu/webshop/controller/UserContextController.java
+++ apps/backend/src/main/java/no/ntnu/webshop/controller/UserContextController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import no.ntnu.webshop.contracts.ErrorResponse;
+import no.ntnu.webshop.contracts.GenericResponse;
 import no.ntnu.webshop.contracts.user.UpdateUserProfileRequest;
 import no.ntnu.webshop.contracts.user.UserProfile;
 import no.ntnu.webshop.repository.UserAccountJpaRepository;
@@ -27,6 +31,7 @@ import no.ntnu.webshop.security.UserAccountDetailsAdapter;
  * @see no.ntnu.webshop.security.AuthenticationRequestFilter Implementation of the filter that is
  *      responsible finding the user account from an access token
  */
+@Slf4j
 @Tag(name = "User Context", description = "Endpoints that apply to the currently logged in user")
 @RestController
 @RequiredArgsConstructor
@@ -63,6 +68,33 @@ public class UserContextController {
     this.userAccountJpaRepository.save(user);
 
     return ResponseEntity.ok(this.userAccountJpaRepository.findProfile(user.getId()));
+  }
+
+  @Operation(
+    summary = "Deletes the user account for the currently logged in user",
+    description = "Permanently deletes the user account for the currently logged in user. This action is irreversible."
+  )
+  @DeleteMapping
+  public ResponseEntity<Object> deleteAccount(
+      @AuthenticationPrincipal UserAccountDetailsAdapter adapter
+  ) {
+    try {
+      this.userAccountJpaRepository.deleteById(adapter.getUserAccount().getId());
+    } catch (Exception e) {
+      log.error("Failed to delete user account: {}", e.getMessage());
+      log.debug("Failed to delete user account", e);
+
+      return ResponseEntity.badRequest()
+        .body(
+          new ErrorResponse(
+            500,
+            "FAILED_TO_DELETE_USER_ACCOUNT",
+            "Failed to delete user account"
+          )
+        );
+    }
+
+    return ResponseEntity.ok(GenericResponse.builder().message("User account deleted").build());
   }
 
 }
