cbf5d200dddbade43f6d4fbf217085954144ec1a
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-04-18 20:32:30 +0200

    fix(ui): #122 close the dialog when clicking outside it

--- packages/ui/src/components/feedback/DialogPrompt.tsx
+++ packages/ui/src/components/feedback/DialogPrompt.tsx
@@ -1,5 +1,6 @@
 import { Dialog, Transition } from '@headlessui/react';
 import Button from '../input/Button';
+import { Fragment } from 'react';
 
 export interface DialogProps {
   isOpen: boolean;
@@ -16,43 +17,51 @@ const DialogPrompt: React.FC<DialogProps> = ({
   message,
   action,
 }) => {
-  function closeModal() {
-    onClose();
-  }
-
   return (
-    <Transition appear show={isOpen}>
-      <Dialog
-        as='div'
-        className='bg-base-900/90 fixed top-0 flex h-full w-full justify-center pt-24'
-        onClose={closeModal}
-      >
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as='div' className='relative z-50' onClose={onClose}>
         <Transition.Child
+          as={Fragment}
           enter='ease-out duration-300'
-          enterFrom='opacity-0 scale-95'
-          enterTo='opacity-100 scale-100'
+          enterFrom='opacity-0'
+          enterTo='opacity-100'
           leave='ease-in duration-200'
-          leaveFrom='opacity-100 scale-100'
-          leaveTo='opacity-0 scale-95'
+          leaveFrom='opacity-100'
+          leaveTo='opacity-0'
         >
-          <div className='border-primary-600 bg-base-900 flex max-w-md flex-col rounded-sm border p-6'>
-            <Dialog.Title
-              as='h3'
-              className='font-title text-xl font-semibold uppercase'
-            >
-              {title}
-            </Dialog.Title>
-            <div className='mt-2'>
-              <p className='text-sm'>{message}</p>
-            </div>
-
-            <div className='flex flex-row justify-between gap-2 pt-4'>
-              <Button onClick={action}>{title}</Button>
-
-              <Button onClick={closeModal}>cancel</Button>
-            </div>
-          </div>
+          <div className='fixed inset-0 bg-[#000]/40' />
         </Transition.Child>
+
+        <div className='fixed inset-0 overflow-y-auto'>
+          <div className='flex min-h-full items-center justify-center p-4 text-center'>
+            <Transition.Child
+              enter='ease-out duration-300'
+              enterFrom='opacity-0 scale-95'
+              enterTo='opacity-100 scale-100'
+              leave='ease-in duration-200'
+              leaveFrom='opacity-100 scale-100'
+              leaveTo='opacity-0 scale-95'
+            >
+              <Dialog.Panel className='border-primary-600 bg-base-900 flex max-w-md flex-col rounded-sm border p-6'>
+                <Dialog.Title
+                  as='h3'
+                  className='font-title text-xl font-semibold uppercase'
+                >
+                  {title}
+                </Dialog.Title>
+                <div className='mt-2'>
+                  <p className='text-sm'>{message}</p>
+                </div>
+
+                <div className='flex flex-row justify-between gap-2 pt-4'>
+                  <Button onClick={action}>{title}</Button>
+
+                  <Button onClick={onClose}>cancel</Button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
       </Dialog>
     </Transition>
   );
