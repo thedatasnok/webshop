95cd824a2041b3a9c7f2d480bdc484f5338c1727
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-04-19 12:19:06 +0200

    fix(backend): #112 use correct annotation for parsing param from request URI

--- apps/backend/src/main/java/no/ntnu/webshop/controller/UserContextOrderController.java
+++ apps/backend/src/main/java/no/ntnu/webshop/controller/UserContextOrderController.java
@@ -8,6 +8,7 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -65,7 +66,7 @@ public class UserContextOrderController {
   @GetMapping("/{orderId}")
   public ResponseEntity<OrderDetails> findOrder(
       @AuthenticationPrincipal UserAccountDetailsAdapter adapter,
-      @RequestParam("orderId") Long orderId
+      @PathVariable Long orderId
   ) {
     var userId = adapter.getUserAccount().getId();
     var results = this.orderJdbcRepository.findOrdersByUserId(userId, Optional.empty(), Optional.of(orderId));
