989cff124a421a01de67a276a4240617da4d1acd
GBFur <zifergames@gmail.com>
2023-05-03 19:46:04 +0200

    test(backend): #142 related product test & refactor

--- apps/backend/src/test/java/no/ntnu/webshop/controller/ProductControllerTests.java
+++ apps/backend/src/test/java/no/ntnu/webshop/controller/ProductControllerTests.java
@@ -14,16 +14,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
+import no.ntnu.webshop.contracts.category.CreateCategoryRequest;
 import no.ntnu.webshop.contracts.product.CreateProductRequest;
 import no.ntnu.webshop.contracts.product.ProductDetails;
+import no.ntnu.webshop.contracts.product.ProductListItem;
+import no.ntnu.webshop.model.Category;
 import no.ntnu.webshop.model.UserAccount;
 import no.ntnu.webshop.model.UserAccountRole;
 import no.ntnu.webshop.repository.ProductJpaRepository;
@@ -77,11 +83,167 @@ class ProductControllerTests {
 
     var countBefore = this.productJpaRepository.count();
 
-    var result = this.mockMvc
+    var result = createProduct(request);
+
+    assertEquals(countBefore + 1, this.productJpaRepository.count());
+
+    var id = this.objectMapper.readValue(result.getResponse().getContentAsString(), ProductDetails.class).id();
+
+    deleteProductWithId(id, userAccount);
+
+    assertFalse(this.productJpaRepository.existsById(id), "Product was not deleted");
+  }
+
+  /**
+   * 
+   * Tests the endpoint to find related products. Creates a test category and three products, one
+   * without category and two with the same category. Asserts that the related product is at the first
+   * position in the list.
+   * 
+   * @throws Exception if an error occurs during the test.
+   */
+  @Test
+  void testFindRelatedProducts() throws Exception {
+    var categoryRequest = new CreateCategoryRequest(
+      "test category",
+      ""
+    );
+    var categoryResult = mockMvc
+      .perform(
+        MockMvcRequestBuilders.post("/api/v1/categories")
+          .contentType(MediaType.APPLICATION_JSON)
+          .header("Authorization", this.authorizationTestUtility.generateJwt(userAccount))
+          .content(objectMapper.writeValueAsString(categoryRequest))
+      )
+      .andExpect(MockMvcResultMatchers.status().isOk())
+      .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(categoryRequest.name()))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.iconUrl").value(categoryRequest.iconUrl()));
+
+    Integer categoryId = objectMapper
+      .readValue(categoryResult.andReturn().getResponse().getContentAsString(), Category.class)
+      .getId();
+
+    var productRequest1 = new CreateProductRequest(
+      "sample product",
+      "",
+      "sample description",
+      "",
+      List.of("https://example.com/image.png"),
+      300.0,
+      false,
+      null,
+      Set.of(),
+      Map.of(),
+      Map.of()
+    );
+
+    var productRequest2 = new CreateProductRequest(
+      "sample product",
+      "",
+      "sample description",
+      "",
+      List.of("https://example.com/image.png"),
+      300.0,
+      false,
+      null,
+      Set.of(categoryId),
+      Map.of(),
+      Map.of()
+    );
+
+    var productRequest3 = new CreateProductRequest(
+      "sample product",
+      "",
+      "sample description",
+      "",
+      List.of("https://example.com/image.png"),
+      300.0,
+      false,
+      null,
+      Set.of(categoryId),
+      Map.of(),
+      Map.of()
+    );
+
+    var productResult1 = createProduct(productRequest1);
+    var productResult2 = createProduct(productRequest2);
+    var productResult3 = createProduct(productRequest3);
+
+    var productId1 = this.objectMapper
+      .readValue(productResult1.getResponse().getContentAsString(), ProductDetails.class)
+      .id();
+    var productId2 = this.objectMapper
+      .readValue(productResult2.getResponse().getContentAsString(), ProductDetails.class)
+      .id();
+    var productId3 = this.objectMapper
+      .readValue(productResult3.getResponse().getContentAsString(), ProductDetails.class)
+      .id();
+
+    // get related products for the second product
+    var relatedProductSecondProduct = mockMvc
+      .perform(
+        MockMvcRequestBuilders.get("/api/v1/products/" + productId2 + "/related")
+          .contentType(MediaType.APPLICATION_JSON)
+          .header("Authorization", this.authorizationTestUtility.generateJwt(userAccount))
+      )
+      .andExpect(MockMvcResultMatchers.status().isOk())
+      .andReturn()
+      .getResponse()
+      .getContentAsString();
+
+    // get the list of related products
+    List<ProductListItem> secondProductRelatedList = objectMapper
+      .readValue(relatedProductSecondProduct, new TypeReference<List<ProductListItem>>(){});
+
+    // asserts that the first related product is the third product
+    // This is because the third product has the same category as the second product
+    assertEquals(secondProductRelatedList.get(0).id(), productId3);
+
+    // does the same for the third product
+    var relatedProductsThirdProduct = mockMvc
+      .perform(
+        MockMvcRequestBuilders.get("/api/v1/products/" + productId3 + "/related")
+          .contentType(MediaType.APPLICATION_JSON)
+          .header("Authorization", this.authorizationTestUtility.generateJwt(userAccount))
+      )
+      .andExpect(MockMvcResultMatchers.status().isOk())
+      .andReturn()
+      .getResponse()
+      .getContentAsString();
+
+    List<ProductListItem> thirdProductRelatedList = objectMapper
+      .readValue(relatedProductsThirdProduct, new TypeReference<List<ProductListItem>>(){});
+
+    assertEquals(thirdProductRelatedList.get(0).id(), productId2);
+
+    // cleanup
+    deleteProductWithId(productId1, userAccount);
+    deleteProductWithId(productId2, userAccount);
+    deleteProductWithId(productId3, userAccount);
+
+    this.mockMvc
+      .perform(
+        MockMvcRequestBuilders.delete("/api/v1/categories/" + categoryId)
+          .header("Authorization", this.authorizationTestUtility.generateJwt(userAccount))
+      )
+      .andExpect(MockMvcResultMatchers.status().isOk());
+  }
+
+  /**
+   * Creates a product and returns the result
+   * 
+   * @param productRequest the product request
+   * @return the result
+   * @throws Exception if an error occurs
+   */
+  private MvcResult createProduct(
+      CreateProductRequest productRequest
+  ) throws Exception {
+    return this.mockMvc
       .perform(
         MockMvcRequestBuilders.post("/api/v1/products")
           .contentType(MediaType.APPLICATION_JSON)
-          .content(this.objectMapper.writeValueAsString(request))
+          .content(this.objectMapper.writeValueAsString(productRequest))
           .header("Authorization", this.authorizationTestUtility.generateJwt(userAccount))
       )
       .andExpect(MockMvcResultMatchers.status().isOk())
@@ -96,19 +258,26 @@ class ProductControllerTests {
       .andExpect(MockMvcResultMatchers.jsonPath("$.attributes").isEmpty())
       .andExpect(MockMvcResultMatchers.jsonPath("$.children").isEmpty())
       .andReturn();
+  }
 
-    assertEquals(countBefore + 1, this.productJpaRepository.count());
-
-    var id = this.objectMapper.readValue(result.getResponse().getContentAsString(), ProductDetails.class).id();
-
-    this.mockMvc
+  /**
+   * Deletes a product with the given id
+   * 
+   * @param id          the id of the product
+   * @param userAccount the user account
+   * @return the product result
+   * @throws Exception if an error occurs
+   */
+  private ResultActions deleteProductWithId(
+      Long id,
+      UserAccount userAccount
+  ) throws Exception {
+    return mockMvc
       .perform(
         MockMvcRequestBuilders.delete("/api/v1/products/" + id + "?force=true")
           .header("Authorization", this.authorizationTestUtility.generateJwt(userAccount))
       )
       .andExpect(MockMvcResultMatchers.status().isOk());
-
-    assertFalse(this.productJpaRepository.existsById(id), "Product was not deleted");
   }
 
 }
