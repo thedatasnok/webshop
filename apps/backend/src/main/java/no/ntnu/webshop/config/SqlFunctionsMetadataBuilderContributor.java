5cc7f6f6ac3e64a07268e066e8fa9ba1368dc77c
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-05-16 17:57:22 +0200

    docs(backend): add comment describing the metadata contributor

--- apps/backend/src/main/java/no/ntnu/webshop/config/SqlFunctionsMetadataBuilderContributor.java
+++ apps/backend/src/main/java/no/ntnu/webshop/config/SqlFunctionsMetadataBuilderContributor.java
@@ -7,6 +7,16 @@ import org.hibernate.type.StandardBasicTypes;
 
 public class SqlFunctionsMetadataBuilderContributor implements MetadataBuilderContributor {
 
+  /**
+   * Contributes to the {@link MetadataBuilder} by adding the SQL function "SIMILARITY" which is
+   * provided by the "pg_trgm" extension in Postgres. This class file is referenced from the
+   * application properties, resulting in it being applied.
+   * 
+   * This essentially allows us to use the SIMILARITY() function in JPQL queries.
+   * 
+   * @see no.ntnu.webshop.repository.ProductJpaRepository#findProducts An example of this function
+   *      being used
+   */
   @Override
   public void contribute(
       MetadataBuilder metadataBuilder
