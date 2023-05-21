9d3ccc615fb463e8e4538f4d55efe0be00de5042
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2023-05-21 16:48:38 +0200

    chore(backend): remove unused repository method

--- apps/backend/src/main/java/no/ntnu/webshop/repository/OrderJpaRepository.java
+++ apps/backend/src/main/java/no/ntnu/webshop/repository/OrderJpaRepository.java
@@ -1,7 +1,5 @@
 package no.ntnu.webshop.repository;
 
-import java.util.Date;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -9,35 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import no.ntnu.webshop.contracts.order.OrderSummary;
 import no.ntnu.webshop.model.Order;
 
 public interface OrderJpaRepository extends JpaRepository<Order, Long> {
-
-  /**
-   * Finds a daily order summary since a given date.
-   * 
-   * @param since the date to start from
-   * 
-   * @return a list of daily order summaries, since the given date
-   * 
-   * @deprecated the version in {@link OrderJdbcRepository} replaces this
-   */
-  @Deprecated
-  @Query("""
-      SELECT new no.ntnu.webshop.contracts.order.OrderSummary(
-        EXTRACT(date FROM o.orderedAt),
-        COUNT(DISTINCT o.id),
-        SUM(ol.subtotal)
-      )
-      FROM Order o
-      JOIN o.lines ol
-      WHERE o.orderedAt >= :since
-      GROUP BY EXTRACT(date FROM o.orderedAt)
-    """)
-  List<OrderSummary> findDailyOrderSummary(
-      @Param("since") Date since
-  );
 
   /**
    * Finds an order by its id and the customers id.
